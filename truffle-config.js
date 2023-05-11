d42fbb4a7fc4a8464498bc513157d2f70bde2eb7
Murakami <101239537+murakamiy12@users.noreply.github.com>
2023-05-11 13:57:01 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -62,6 +62,18 @@ module.exports = {
       gasPrice: 10000000000, // 10 gwei
       skipDryRun: true
     },
+    sepolia: {
+      provider: function() {
+        return new HDWalletProvider(
+          MNEMONIC,
+          `https://sepolia.infura.io/v3/${API_KEY}`
+        )
+      },
+      network_id: 11155111,
+      gas: 8000000,
+      gasPrice: 10000000000, // 10 gwei
+      skipDryRun: true
+    },
     mainnet: {
       provider: function() {
         return new HDWalletProvider(
